01774e2c9e3db4a6dababfa54d7cd690449fc474
HIRUN <hirundesilva97@gmail.com>
2025-02-26 10:37:25 -0500

    CHANGE: Added comments for Task 4 and Task 5

--- cc_12.js
+++ cc_12.js
@@ -19,47 +19,47 @@ arrayOfMetricCards.forEach(card => {
 );
 
 //Task 3 - Implemented Dynamic Inventory List
-const inventoryList = document.getElementById("inventoryList")
-function addInventoryItem(productName) {
+const inventoryList = document.getElementById("inventoryList")  //Nested structure inventoryList
+function addInventoryItem(productName) {    //Function that creates a new <li> element representing the product
   const li = document.createElement("li")
-  li.setAttribute('class', "product-item")
+  li.setAttribute('class', "product-item")  //setAttribute to add class product-item
   li.textContent = productName 
-  li.addEventListener("click", function() {
+  li.addEventListener("click", function() {   //Seperate function that removes specific <li> when clicked
     removeInventoryItem(li)
-  })
-inventoryList.appendChild(li)
+  });
+inventoryList.appendChild(li)   //Append li to the inventoryList.
 };
 
 function removeInventoryItem(item) {
-  inventoryList.removeChild(item);
+  inventoryList.removeChild(item);    //Use removeChild within the inventory list
 };
 
 addInventoryItem("Phone");
 
 //Task 4 - Demonstrated Event Bubbling in Customer Section
-const customerSection = document.getElementById("customerSection")
+const customerSection = document.getElementById("customerSection")  //Nested structure with parent container "customerSection"
 
 const customerCard1 = document.createElement("div");
-customerCard1.setAttribute("class", "customer-card");
+customerCard1.setAttribute("class", "customer-card");   //Child element "class","customer-card"
 customerCard1.textContent = "Customer 1";
-customerSection.appendChild(customerCard1);
+customerSection.appendChild(customerCard1);   //Appending customerCard1 to customerSection
 
 const customerCard2 = document.createElement("div");
-customerCard2.setAttribute("class", "customer-card");
+customerCard2.setAttribute("class", "customer-card");    //Child element "class", "customer-card"
 customerCard2.textContent = "Customer 2";
-customerSection.appendChild(customerCard2);
+customerSection.appendChild(customerCard2);   //Appending customerCard2 to customerSection
 
 customerSection.addEventListener("click", function() {
-  console.log("Customer section was clicked")
+  console.log("Customer section was clicked")   //Log message when customer section is clicked
 });
 
 document.querySelectorAll(".customer-card")
 .forEach(card => {
   card.addEventListener("click", function(event) {
-    console.log("Customer card clicked")
+    console.log("Customer card clicked")      //Log message when customer card is clicked
     event.stopPropagation();
-  })
-})
+  });
+});
 
 
 
